635baf50fe6e50ac243378ee63a0e1e04df3214e
millson505 <millson505@gmail.com>
2026-06-18 21:15:54 +0900

    Update hourly.py

--- hourly.py
+++ hourly.py
@@ -1,1 +1,25 @@
-print("hourly")
+import os
+import requests
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+message = """
+📈 Funding Monitor
+
+HYPE
+Funding: 테스트
+
+HYNIX
+Funding: 테스트
+"""
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+requests.post(
+    url,
+    json={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+)
