d24a7498e6fd5574ef6246b65aaa5f7a9e449cd6
millson505 <millson505@gmail.com>
2026-06-18 22:52:29 +0900

    Update daily.py

--- daily.py
+++ daily.py
@@ -1,7 +1,10 @@
 import requests
 
-r = requests.get("https://sosovalue.com/_next/data/KWchX1um_BW_mW00I76Pl/assets/etf/us-hype-spot.json")
+r = requests.get(
+    "https://sosovalue.com/_next/data/KWchX1um_BW_mW00I76Pl/assets/etf/us-hype-spot.json"
+)
 
-print(type(r.json()))
-print(r.json())
-exit()
+data = r.json()
+
+print(data.keys())
+print(data["pageProps"].keys())
