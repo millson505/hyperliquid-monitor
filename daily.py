5a09f31978e23ed01c04771f638aebe28a0b8696
millson505 <millson505@gmail.com>
2026-06-18 21:22:59 +0900

    Update daily.py

--- daily.py
+++ daily.py
@@ -4,22 +4,14 @@ import requests
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
 
-message = """
-📊 Hyperliquid Daily
-
-Revenue: 조회예정
-Buyback: 조회예정
-
-HYPE ETF Flow: 조회예정
-7D ETF Flow: 조회예정
-"""
-
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-requests.post(
+r = requests.post(
     url,
     json={
         "chat_id": CHAT_ID,
-        "text": message
+        "text": "테스트"
     }
 )
+
+print(r.text)
